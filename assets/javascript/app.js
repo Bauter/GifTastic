c2ab367041e24c86580dd805820c93efaed5a744
Bauter <bauter09@gmail.com>
2020-01-15 14:10:27 -0600

    Files set up, basic HTML format created, app.js pseudo coding started

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+// Define global variables and themed Array.
+
+// Use "GET" method to access Giphy API.
+
+// Create Buttons for each item in Array and input to DOM.
+
+// Grab user input and onClick to add input to Array.
+
+// Input loaded Giphy images to -'giphy-div' with DOM manipulation.
