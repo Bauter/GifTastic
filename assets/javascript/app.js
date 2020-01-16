ab18c43425ac66f954f37c04e835da51fbe76f4b
Bauter <bauter09@gmail.com>
2020-01-16 00:25:37 -0600

    ajax method set up and pulling from giphy api, buggy click function, new buttons in-op, needs debugging

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,51 +1,73 @@
 // Define global variables and themed Array.
-let carArray = ["ae86", "is300", "supra", "rx7", "180sx"];
-let car;
-let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + car + "&limit=10&rating=g&api_key=iteCBHWxKTj06159RB4MDy59EObpbeRg";
+let carArray = ["ae86", "silvia", "supra", "rx7", "180sx"];
+let car;// = $('#gif-search').val();
+//let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + car + "&limit=10&rating=g&api_key=iteCBHWxKTj06159RB4MDy59EObpbeRg";
 
-// Use "GET" method to access Giphy API.
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(response) {
-    console.log(response);
-});
+
+$(document).ready(() => {
 // Create Buttons for each item in Array and input to DOM.
-function createButtons() {
+    function generateButtons() {
 
-    $('#button-div').empty();
+        $("#button-div").empty();
 
-    for(var i =0; i<carArray.length; i++) {
-        // create button variable
-        let btn = $('<button>');
-        // Adding class
-        btn.addClass("gif");
-        // Adding data-attr.
-        btn.attr("data-name", carArray[i]);
-        // Input text to button
-        btn.text(carArray[i]);
-        // Append new button to div
-        $('#button-div').append(btn);
-    }
-    
+        for (var i = 0; i < carArray.length; i++) {
+            
+            let btn = $("<button>");
+            btn.addClass("gif-button");
+            btn.attr("data-name", carArray[i]);
+            btn.text(carArray[i]);
+            $("#button-div").append(btn);
+        };
+    };
 
 
+    // Grab user input and onClick to add input to Array.
+    $("#add-gif").on("click", function(event) {
+        event.preventDefault();
+        let car = $("#gif-search").val();
+        carArray.push(car);
+        
+        generateButtons()
+        
+    });
+
+    generateButtons()
+
+    // Use "GET" method to access Giphy API.
+    $(".gif-button").on("click", function() {
+
+        let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + car + "&limit=10&rating=g&api_key=iteCBHWxKTj06159RB4MDy59EObpbeRg";
+        car = $(this).attr("data-name");
+
+        $.ajax({
+        url: queryURL,
+        method: "GET"
+        }).then(function(response) {
+            console.log(response);
+            let results = response.data;
+            
+            for ( var j = 0; j < results.length; j++) {
+                let imgDiv = $("<div>");
+                let rating = results[j].rating;
+                let paragraph = $("<p>");
+                paragraph.text("rating: " + rating);
+                let gifImg = $("<img>");
+                gifImg.attr("src", results[j].images.fixed_height.url);
+                imgDiv.prepend(paragraph);
+                imgDiv.prepend(gifImg);
+                $("#giphy-div").prepend(imgDiv);
+            };
 
 
-}
-// Grab user input and onClick to add input to Array.
-$('#add-gif').on('click', function(event) {
-    event.preventDefault();
-    // Grab text from input
-    car = $('#gif-search').val().trim().toLowerCase();
-    // car is added to array
-    carArray.push(car);
 
 
-    createButtons();
-    
-})
 
-createButtons();
+            
+        }); 
+    });
+
+
+
+});
 
 // Input loaded Giphy images to -'giphy-div' with DOM manipulation.
